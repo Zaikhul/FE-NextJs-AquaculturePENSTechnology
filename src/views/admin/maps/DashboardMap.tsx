8abd817a289cfdc77ee4c6669227c83dc20333d8
santisans123 <santitok21@gmail.com>
2024-12-10 16:40:17 +0700

    update production

--- src/views/admin/maps/DashboardMap.tsx
+++ src/views/admin/maps/DashboardMap.tsx
@@ -1,46 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
 
 type positionDataType = {
-	id: number;
-	name: string;
-	lat: number;
-	lng: number;
+    id: number;
+    name: string;
+    lat: number;
+    lng: number;
 }
 
 function DashboardMap() {
+    const [positionData, setPositionData] = useState<positionDataType[]>([]);
+    
+    // Define center position with correct type
+    const defaultCenter: LatLngExpression = [0, 0];
 
-	const [positionData, setPositionData] = useState([]);
+    useEffect(() => {}, []);
 
-	useEffect(() => {}, []);
-
-	return (
-		<MapContainer center={[0, 0]} zoom={2} style={{ height: '500px', width: '100%' }}>
-			<TileLayer
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.webp"
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-			/>
-			{positionData.map((position: positionDataType) => (
-				<Marker key={position.id} position={[position.lat, position.lng]}>
-					<Popup>
-						{position.name}
-					</Popup>
-				</Marker>
-			))}
-		</MapContainer>
-	)
+    return (
+        <MapContainer 
+            center={defaultCenter}
+            zoom={2} 
+            style={{ height: '500px', width: '100%' }}
+            scrollWheelZoom={false}
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.webp"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {positionData.map((position: positionDataType) => {
+                const markerPosition: LatLngExpression = [position.lat, position.lng];
+                return (
+                    <Marker key={position.id} position={markerPosition}>
+                        <Popup>
+                            {position.name}
+                        </Popup>
+                    </Marker>
+                );
+            })}
+        </MapContainer>
+    );
 }
 
-export default DashboardMap
-
-// // components/MapComponent.js
-
-// const MapComponent = () => {
-
-
-//   return (
-
-//   );
-// };
-
-// export default MapComponent;
+export default DashboardMap;
